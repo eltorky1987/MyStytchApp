3bef3ff88e5df81578afda16f5105952688a5132
eltorky1987 <m2117513495@gmail.com>
2026-03-22 02:19:16 +0200

    Finish my Digital Store app

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class MainActivity {
     public static void main(String[] args) {
-        String devName = "Agina";
-        System.out.println("Hello World!");
-        System.out.println("Developer: " + devName);
-        System.out.println("Status: System is Online on Termux.");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("🛒 --- Agina Digital Store --- 🛒");
+        
+        System.out.print("📦 Enter Product Name: ");
+        String product = input.nextLine();
+        
+        System.out.print("💰 Enter Unit Price (EGP): ");
+        double price = input.nextDouble();
+        
+        System.out.print("🔢 Enter Quantity: ");
+        int qty = input.nextInt();
+        
+        double total = price * qty;
+        
+        System.out.println("\n----------------------------");
+        System.out.println("🧾 INVOICE FOR: " + product);
+        System.out.println("💵 Price: " + price + " EGP");
+        System.out.println("📦 Quantity: " + qty);
+        System.out.println("----------------------------");
+        System.out.println("💎 TOTAL AMOUNT: " + total + " EGP");
+        System.out.println("----------------------------");
+        System.out.println("Developer: Agina tork");
     }
 }
